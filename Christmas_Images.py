ea35b7bdef340cf0243ade037c73c93134f49770
Dylan Stitt <99621158+dylanstitt@users.noreply.github.com>
2024-09-23 12:49:10 -0400

    Add files via upload
    final - python 2.7

---
+++ Christmas_Images.py
@@ -0,0 +1,194 @@
+# Colors might not match up to real rgb values
+n = (0, 0, 0)
+w = (200, 100, 255)
+r = (255, 0, 0)
+o = (255, 165, 0)
+g = (0, 255, 0)
+b = (0, 0, 255)
+y = (255, 200, 0)
+gr = (128, 128, 128)
+br = (180,100,14)
+bl = (0, 0, 0)
+
+template = [
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+          [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w]]
+
+fire = [
+          [gr, gr, gr, gr, gr, gr, r, r, gr, gr, r, r, gr, gr, gr, gr],
+          [gr, gr, gr, gr, gr, gr, r, r, gr, gr, r, r, gr, gr, gr, gr],
+          [gr, gr, gr, gr, r, r, o, o, r, r, r, r, r, r, gr, gr],
+          [gr, gr, gr, gr, r, r, o, o, r, r, r, r, r, r, gr, gr],
+          [gr, gr, gr, gr, r, r, o, o, o, o, o, o, o, o, r, r],
+          [gr, gr, gr, gr, r, r, o, o, o, o, o, o, o, o, r, r],
+          [gr, gr, r, r, o, o, o, o, y, y, y, y, br, br, r, r],
+          [gr, gr, r, r, o, o, o, o, y, y, y, y, br, br, r, r],
+          [gr, gr, r, r, o, o, y, y, br, br, y, y, br, br, br, br],
+          [gr, gr, r, r, o, o, y, y, br, br, y, y, br, br, br, br],
+          [gr, gr, r, r, o, o, br, br, br, br, bl, bl, br, br, br, br],
+          [gr, gr, r, r, o, o, br, br, br, br, bl, bl, br, br, br, br],
+          [r, r, r, r, br, br, br, br, bl, bl, bl, bl, br, br, br, br],
+          [r, r, r, r, br, br, br, br, bl, bl, bl, bl, br, br, br, br],
+          [r, r, br, br, bl, bl, br, br, br, br, br, br, br, br, br, br],
+          [r, r, br, br, bl, bl, br, br, br, br, br, br, br, br, br, br]]
+
+snowman1 = [
+            [b, b, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+            [b, b, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+            [w, w, w, w, bl, bl, w, w, w, w, w, w, bl, bl, w, w],
+            [w, w, w, w, bl, bl, w, w, w, w, w, w, bl, bl, w, w],
+            [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+            [w, w, w, w, w, w, w, w, w, w, w, w, w, w, w, w],
+            [w, w, w, w, w, w, o, o, o, o, w, w, w, w, w, w],
+            [w, w, w, w, w, w, o, o, o, o, w, w, w, w, w, w],
+            [w, w, w, w, w, w, o, o, o, o, o, o, o, o, w, w],
+            [w, w, w, w, w, w, o, o, o, o, o, o, o, o, w, w],
+            [w, w, bl, bl, w, w, w, w, w, w, w, w, w, w, bl, bl],
+            [w, w, bl, bl, w, w, w, w, w, w, w, w, w, w, bl, bl],
+            [b, b, w, w, w, w, bl, bl, w, w, bl, bl, w, w, w, w],
+            [b, b, w, w, w, w, bl, bl, w, w, bl, bl, w, w, w, w],
+            [b, b, b, b, r, r, r, r, r, r, r, r, r, r, r, r],
+            [b, b, b, b, r, r, r, r, r, r, r, r, r, r, r, r],]
+
+candyCane1 = [
+          [b, b, b, b, b, b, b, b, b, b, w, w, b, b, b, b],
+          [b, b, b, b, b, b, b, b, b, b, w, w, b, b, b, b],
+          [b, b, b, b, b, b, b, b, r, r, b, b, r, r, b, b],
+          [b, b, b, b, b, b, b, b, r, r, b, b, r, r, b, b],
+          [b, b, b, b, b, b, b, b, b, b, w, w, b, b, b, b],
+          [b, b, b, b, b, b, b, b, b, b, w, w, b, b, b, b],
+          [b, b, b, b, b, b, b, b, r, r, b, b, b, b, b, b],
+          [b, b, b, b, b, b, b, b, r, r, b, b, b, b, b, b],
+          [b, b, b, b, b, b, w, w, b, b, b, b, b, b, b, b],
+          [b, b, b, b, b, b, w, w, b, b, b, b, b, b, b, b],
+          [b, b, b, b, r, r, b, b, b, b, b, b, b, b, b, b],
+          [b, b, b, b, r, r, b, b, b, b, b, b, b, b, b, b],
+          [b, b, w, w, b, b, b, b, b, b, b, b, b, b, b, b],
+          [b, b, w, w, b, b, b, b, b, b, b, b, b, b, b, b],
+          [r, r, b, b, b, b, b, b, b, b, b, b, b, b, b, b],
+          [r, r, b, b, b, b, b, b, b, b, b, b, b, b, b, b]]
+
+candyCane2 = [
+          [n, n, n, n, g, g, r, r, g, g, n, n, n, n, n, n],
+          [n, n, n, n, g, g, r, r, g, g, n, n, n, n, n, n],
+          [n, n, r, r, g, g, r, r, g, g, r, r, n, n, n, n],
+          [n, n, r, r, g, g, r, r, g, g, r, r, n, n, n, n],
+          [n, n, r, r, r, r, n, n, r, r, r, r, n, n, n, n],
+          [n, n, r, r, r, r, n, n, r, r, r, r, n, n, n, n],
+          [n, n, n, n, n, n, n, n, g, g, g, g, n, n, n, n],
+          [n, n, n, n, n, n, n, n, g, g, g, g, n, n, n, n],
+          [n, n, n, n, n, n, r, r, r, r, n, n, n, n, n, n],
+          [n, n, n, n, n, n, r, r, r, r, n, n, n, n, n, n],
+          [n, n, n, n, n, n, g, g, g, g, n, n, n, n, n, n],
+          [n, n, n, n, n, n, g, g, g, g, n, n, n, n, n, n],
+          [n, n, n, n, r, r, r, r, n, n, n, n, n, n, n, n],
+          [n, n, n, n, r, r, r, r, n, n, n, n, n, n, n, n],
+          [n, n, n, n, g, g, g, g, n, n, n, n, n, n, n, n],
+          [n, n, n, n, g, g, g, g, n, n, n, n, n, n, n, n]]
+
+star = [
+        [b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b],
+        [b, b, b, b, b, b, bl, bl, bl, bl, b, b, b, b, b, b],
+        [b, b, b, b, b, bl, y, y, y, y, bl, b, b, b, b, b],
+        [b, b, b, b, b, bl, y, y, y, y, bl, b, b, b, b, b],
+        [b, b, b, b, b, bl, y, y, y, y, bl, b, b, b, b, b],
+        [b, bl, bl, bl, bl, bl, y, y, y, y, bl, bl, bl, bl, bl, b],
+        [b, bl, y, y, y, y, y, y, y, y, y, y, y, y, bl, b],
+        [b, bl, y, y, y, y, y, y, y, y, y, y, y, y, bl, b],
+        [b, b, b, bl, y, y, y, y, y, y, y, y, bl, b, b, b],
+        [b, b, b, bl, y, y, y, y, y, y, y, y, bl, b, b, b],
+        [b, bl, y, y, y, y, y, y, y, y, y, y, y, y, bl, b],
+        [b, bl, y, y, y, y, y, y, y, y, y, y, y, y, bl, b],
+        [b, bl, y, y, y, y, bl, bl, bl, bl, y, y, y, y, bl, b],
+        [b, bl, y, y, y, y, bl, b, b, bl, y, y, y, y, bl, b],
+        [b, bl, bl, bl, bl, bl, b, b, b, b, bl, bl, bl, bl, bl, b],
+        [b, b, b, b, b, b, b, b, b, b, b, b, b, b, b, b],]
+
+tree = [
+        [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
+        [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
+        [n, n, n, n, n, n, n, g, g, n, n, n, n, n, n, n],
+        [n, n, n, n, n, n, y, y, y, g, n, n, n, n, n, n],
+        [n, n, n, n, n, n, g, r, y, y, n, n, n, n, n, n],
+        [n, n, n, n, n, g, g, g, g, g, g, n, n, n, n, n],
+        [n, n, n, n, n, y, y, g, g, g, g, n, n, n, n, n],
+        [n, n, n, n, g, r, y, y, y, r, g, g, n, n, n, n],
+        [n, n, n, n, g, g, g, r, y, y, y, y, n, n, n, n],
+        [n, n, n, y, g, g, g, g, g, g, g, g, g, n, n, n],
+        [n, n, n, y, y, y, y, g, g, g, g, g, g, n, n, n],
+        [n, n, g, g, r, g, y, y, y, y, r, g, g, g, n, n],
+        [n, n, g, g, g, g, g, g, g, y, y, y, y, y, n, n],
+        [n, g, g, g, g, g, g, r, g, g, g, g, r, g, g, n],
+        [n, g, g, g, g, g, g, g, g, g, g, g, g, g, g, n],
+        [n, n, n, n, n, n, n, br, br, n, n, n, n, n, n, n]]
+
+
+snowflake = [
+        [n, n, n, n, n, n, n, w, w, n, n, n, n, n, n, n],
+        [n, n, n, n, n, w, n, w, w, n, w, n, n, n, n, n],
+        [n, n, n, n, n, n, w, w, w, w, n, n, n, n, n, n],
+        [n, w, n, n, n, n, n, w, w, n, n, n, n, n, w, n],
+        [w, w, n, w, n, n, w, w, w, w, n, n, w, n, w, w],
+        [n, n, w, w, n, w, w, w, w, w, w, n, w, w, n, n],
+        [n, n, w, w, n, w, w, w, w, w, w, n, w, w, n, n],
+        [n, w, w, w, w, w, n, w, w, n, w, w, w, w, w, n],
+        [n, n, n, w, w, n, w, w, w, w, n, w, w, n, n, n],
+        [n, n, n, w, w, n, w, w, w, w, n, w, w, n, n, n],
+        [n, w, w, w, w, w, n, w, w, n, w, w, w, w, w, n],
+        [n, n, w, w, n, w, w, w, w, w, w, n, w, w, n, n],
+        [w, w, n, w, n, n, w, w, w, w, n, n, w, n, w, w],
+        [n, w, n, n, n, n, n, w, w, n, n, n, n, n, w, n],
+        [n, n, n, n, n, n, w, w, w, w, n, n, n, n, n, n],
+        [n, n, n, n, n, w, n, w, w, n, w, n, n, n, n, n],
+        [n, n, n, n, n, n, n, w, w, n, n, n, n, n, n, n]]
+
+
+present = [
+        [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n],
+        [n, n, n, b, b, b, n, n, n, n, b, b, b, n, n, n],
+        [n, n, b, b, n, n, b, b, b, b, n, n, b, b, n, n],
+        [n, n, b, n, n, n, b, b, b, b, n, n, n, b, n, n],
+        [n, n, n, b, b, b, b, b, b, b, b, b, b, n, n, n],
+        [n, n, o, o, o, o, o, b, b, o, o, o, o, o, n, n],
+        [n, n, o, o, o, o, o, b, b, o, o, o, o, o, n, n],
+        [n, n, o, o, o, o, o, b, b, o, o, o, o, o, n, n],
+        [n, n, o, o, o, o, o, b, b, o, o, o, o, o, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, o, o, o, o, b, b, o, o, o, o, n, n, n],
+        [n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n]]
+
+snowman2 = [
+            [n, n, n, n, n, n, g, r, r, r, r, r, n, n, n, n],
+            [n, n, n, n, w, w, w, g, r, r, r, w, w, n, n, n],
+            [n, n, n, w, w, w, w, w, g, r, n, w, w, n, n, n],
+            [n, n, n, w, bl, w, bl, w, w, g, n, n, n, n, n, n],
+            [n, n, o, o, o, o, w, w, w, w, n, n, n, n, n, n],
+            [n, n, n, w, w, w, w, w, w, w, n, n, n, n, n, n],
+            [n, n, n, n, w, w, w, w, w, n, n, n, n, n, n, n],
+            [n, n, n, n, r, r, r, r, r, r, n, n, n, n, n, n],
+            [n, n, n, r, r, r, r, r, r, r, r, n, n, n, n, n],
+            [n, n, n, w, w, w, w, w, r, r, w, n, n, n, n, n],
+            [n, n, w, w, w, bl, w, y, w, r, r, w, n, n, n, n],
+            [n, n, w, w, bl, w, w, w, w, r, r, w, n, n, n, n],
+            [n, n, w, w, bl, w, w, y, w, w, w, w, n, n, n, n],
+            [n, n, w, w, w, w, w, w, w, w, w, w, n, n, n, n],
+            [n, n, n, w, w, w, w, w, w, w, w, n, n, n, n, n],
+            [n, n, n, n, n, w, w, w, w, w, n, n, n, n, n, n],]
---
+++ main.py
@@ -0,0 +1,74 @@
+# Mrs. H found out how to connect to the board
+# Worked on by: Ryan Stone, Aaron Spink, Dylan Stitt, Graeson Brickner
+
+# To run:
+    # cd to the file in terminal (use ls if you need a list of directories)
+        # Note: You have to add single quotes around anything that has a space
+    # sudo python3 WorkingLed.py
+# To run again:
+    # You can hit up arrow to retype run code
+
+# Make An Import For Each File You Want Images From
+    # Ex. from fileName import *
+from Christmas_Images import *
+
+
+from rpi_ws281x import *
+import time
+
+
+# Any Images That You Want Displayed Must Be Typed Here
+IMAGES = [snowman2, tree, snowflake, present, star, candyCane2, candyCane1, snowman1, fire]
+IMAGE_DELAY = 8
+
+
+LED_COUNT = 256
+LED_PIN = 18
+LED_FREQ_HZ = 800000
+LED_DMA = 10
+LED_BRIGHTNESS = 255
+LED_INVERT = False
+LED_CHANNEL = 0
+
+
+def boardOff(strip):
+    for x in range(LED_COUNT):
+        strip.setPixelColor(x, Color(0, 0, 0))
+    strip.show()
+    
+
+def functionalList(listName):
+        newList = []
+        for x, row in enumerate(listName):
+            if x % 2 == 1:
+                newList.append(row)
+            else:
+                row.reverse()
+                newList.append(row)
+        return newList
+
+
+def display(strip, image):
+    counter = 0
+    for row in image:
+        for colorTuple in row:
+            strip.setPixelColor(counter, Color(colorTuple[0], colorTuple[1], colorTuple[2]))
+            counter += 1
+    strip.show()
+
+
+def main():
+    strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+    strip.begin()
+    
+    for image in IMAGES:
+        image = functionalList(image)
+    
+    while True:
+        for image in IMAGES:
+                display(strip, image)
+                time.sleep(IMAGE_DELAY)
+
+
+if __name__ == "__main__":
+    main()
